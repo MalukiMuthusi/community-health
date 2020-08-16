8e7ede0b406e27efb36710af90a1388c41f6239f
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-08-16 23:41:10 +0300

    Worked data input fragment, created the form for inputing patient data.
    The patient data is collected through a form and submitted to the cloud database.

--- app/src/main/java/codes/malukimuthusi/commhealth/dataInput/BasicFormFragment.kt
+++ app/src/main/java/codes/malukimuthusi/commhealth/dataInput/BasicFormFragment.kt
@@ -6,21 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import codes.malukimuthusi.commhealth.R
+import codes.malukimuthusi.commhealth.databinding.BasicFormFragmentBinding
 
 class BasicFormFragment : Fragment() {
+
 
     companion object {
         fun newInstance() = BasicFormFragment()
     }
 
     private lateinit var viewModel: BasicFormViewModel
+    lateinit var binding: BasicFormFragmentBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.basic_form_fragment, container, false)
+        binding = BasicFormFragmentBinding.inflate(inflater, container, false)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.gender_array,
+            android.R.layout.simple_spinner_item
+        ).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.genderInputText.setAdapter(it)
+        }
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
