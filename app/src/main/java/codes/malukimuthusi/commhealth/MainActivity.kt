1629089f76272bbd1e541ce2ee0e316e41f06d24
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-08-16 16:23:53 +0300

    init repo

---
+++ app/src/main/java/codes/malukimuthusi/commhealth/MainActivity.kt
@@ -0,0 +1,11 @@
+package codes.malukimuthusi.commhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
