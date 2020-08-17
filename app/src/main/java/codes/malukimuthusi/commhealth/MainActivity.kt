e4b18bb5155a9de682f9455ee723198bf6cfc2e1
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-08-17 23:29:19 +0300

    Added Bottom Navigation.

--- app/src/main/java/codes/malukimuthusi/commhealth/MainActivity.kt
+++ app/src/main/java/codes/malukimuthusi/commhealth/MainActivity.kt
@@ -2,10 +2,55 @@ package codes.malukimuthusi.commhealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import codes.malukimuthusi.commhealth.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.selectedItemId = R.id.item_data_input
+
+        BottomNavigationView.OnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_activity -> {
+                    true
+                }
+                R.id.item_call -> {
+                    true
+                }
+                R.id.item_disease -> {
+                    true
+                }
+                R.id.item_data_input -> {
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.setOnNavigationItemReselectedListener {
+            when (it.itemId) {
+                R.id.item_activity -> {
+                    true
+                }
+                R.id.item_call -> {
+                    true
+                }
+                R.id.item_disease -> {
+                    true
+                }
+                R.id.item_data_input -> {
+                    true
+                }
+            }
+        }
+
     }
+
 }
