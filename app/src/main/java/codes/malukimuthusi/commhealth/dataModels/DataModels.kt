e127a634e312f5d39c8cb9b2dde65b21c3ee2a7c
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-08-18 11:36:51 +0300

    Connected app with firebase and changed disease page

---
+++ app/src/main/java/codes/malukimuthusi/commhealth/dataModels/DataModels.kt
@@ -0,0 +1,11 @@
+package codes.malukimuthusi.commhealth.dataModels
+
+import com.google.android.material.bottomappbar.BottomAppBarTopEdgeTreatment
+
+data class Disease(
+    val name: String? = "",
+    val prevention: String? = "",
+    val symptoms: String? = "",
+    val treatment: String? = "",
+    val url: String? = ""
+)
