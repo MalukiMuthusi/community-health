ba6467e6b980857003b03711ca14577cf75a8bd6
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-08-20 20:01:06 +0300

    Twiked the National Disease list, to correspond to master detail view.
    Also added title to the help line numbers screen.

--- app/src/main/java/codes/malukimuthusi/commhealth/dataModels/DataModels.kt
+++ app/src/main/java/codes/malukimuthusi/commhealth/dataModels/DataModels.kt
@@ -1,13 +1,15 @@
 package codes.malukimuthusi.commhealth.dataModels
 
 import com.google.android.material.bottomappbar.BottomAppBarTopEdgeTreatment
+import com.google.firebase.firestore.DocumentReference
 
 data class Disease(
     val name: String? = "",
     val prevention: String? = "",
     val symptoms: String? = "",
     val treatment: String? = "",
-    val url: String? = ""
+    val url: String? = "",
+    var diseaseReference: DocumentReference? = null
 )
 
 data class PhoneNumbers(
--- app/src/main/java/codes/malukimuthusi/commhealth/nationalDiseaseBurdenList/NationalDiseaseBurdenListFragment.kt
+++ app/src/main/java/codes/malukimuthusi/commhealth/nationalDiseaseBurdenList/NationalDiseaseBurdenListFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import codes.malukimuthusi.commhealth.dataModels.Disease
 import codes.malukimuthusi.commhealth.databinding.NationalDiseaseBurdenListFragmentBinding
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.firebase.ui.firestore.SnapshotParser
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 
 class NationalDiseaseBurdenListFragment : Fragment() {
@@ -19,7 +22,7 @@ class NationalDiseaseBurdenListFragment : Fragment() {
             NationalDiseaseBurdenListFragment()
     }
 
-    private lateinit var viewModel: NationalDiseaseBurdenListViewModel
+    private val viewModel: NationalDiseaseBurdenListViewModel by activityViewModels()
     private lateinit var binding: NationalDiseaseBurdenListFragmentBinding
     lateinit var db: FirebaseFirestore
 
@@ -34,7 +37,17 @@ class NationalDiseaseBurdenListFragment : Fragment() {
             .limit(50)
 
         val firestoreRecyclerOptions = FirestoreRecyclerOptions.Builder<Disease>()
-            .setQuery(query, Disease::class.java)
+            .setQuery(query, object : SnapshotParser<Disease> {
+
+                override fun parseSnapshot(snapshot: DocumentSnapshot): Disease {
+                    val disease = snapshot.toObject(Disease::class.java)
+                    if (disease != null) {
+                        disease.diseaseReference = snapshot.reference
+                        return disease
+                    }
+                    return Disease()
+                }
+            })
             .setLifecycleOwner(this)
             .build()
 
@@ -54,7 +67,7 @@ class NationalDiseaseBurdenListFragment : Fragment() {
                 position: Int,
                 model: Disease
             ) {
-                holder.bind(model)
+                holder.bind(model, viewModel)
 
             }
         }
