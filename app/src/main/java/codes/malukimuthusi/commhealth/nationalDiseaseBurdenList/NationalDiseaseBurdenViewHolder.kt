4d713448743104446877b9a7795c1a22ee4c1fcf
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-08-20 19:21:00 +0300

    Created the list of national disease burden.

--- app/src/main/java/codes/malukimuthusi/commhealth/nationalDiseaseBurdenList/NationalDiseaseBurdenViewHolder.kt
+++ app/src/main/java/codes/malukimuthusi/commhealth/nationalDiseaseBurdenList/NationalDiseaseBurdenViewHolder.kt
@@ -2,15 +2,22 @@ package codes.malukimuthusi.commhealth.nationalDiseaseBurdenList
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import codes.malukimuthusi.commhealth.dataModels.Disease
 import codes.malukimuthusi.commhealth.databinding.NationalDiseaseBurdenSingleItemBinding
 
 class NationalDiseaseBurdenViewHolder private constructor(
     private val binding: NationalDiseaseBurdenSingleItemBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(diseaseName: String) {
+    fun bind(diseaseName: Disease) {
         binding.disease = diseaseName
+
+        binding.diseaseName.setOnClickListener {
+            binding.root.findNavController()
+                .navigate(NationalDiseaseBurdenListFragmentDirections.actionNationalDiseaseBurdenListFragmentToDiseaseFragment())
+        }
     }
 
     companion object {
