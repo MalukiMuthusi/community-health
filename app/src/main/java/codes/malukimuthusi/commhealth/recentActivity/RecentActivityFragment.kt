65ddb61e3436a98e618afebc78e96c20af8c23eb
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-08-29 19:53:29 +0300

    Implement count interactions.

--- app/src/main/java/codes/malukimuthusi/commhealth/recentActivity/RecentActivityFragment.kt
+++ app/src/main/java/codes/malukimuthusi/commhealth/recentActivity/RecentActivityFragment.kt
@@ -3,15 +3,15 @@ package codes.malukimuthusi.commhealth.recentActivity
 import android.content.Context
 import android.content.SharedPreferences
 import android.net.Uri
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import codes.malukimuthusi.commhealth.MainActivity
-import codes.malukimuthusi.commhealth.R
 import codes.malukimuthusi.commhealth.databinding.RecentActivityFragmentBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 class RecentActivityFragment : Fragment() {
 
@@ -22,14 +22,20 @@ class RecentActivityFragment : Fragment() {
     private lateinit var viewModel: RecentActivityViewModel
     private lateinit var binding: RecentActivityFragmentBinding
     private lateinit var sharedPref: SharedPreferences
+    lateinit var db: FirebaseFirestore
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = RecentActivityFragmentBinding.inflate(inflater, container, false)
-
+        db = FirebaseFirestore.getInstance()
         sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
+
+        countTodaysInteractions()
+        countWeeklyInteractionss()
+        countMontlyInteractions()
 
         val uri = sharedPref.getString(MainActivity.PROFILE_IMAGE_URI, "")
         if (!uri.isNullOrBlank()) {
@@ -40,6 +46,40 @@ class RecentActivityFragment : Fragment() {
         binding.locationText.text = sharedPref.getString(MainActivity.SHARED_PREF_LOCATION, "")
 
         return binding.root
+    }
+
+    private fun countTodaysInteractions() {
+        val yesterday = Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000)
+        db.collection("patient-records")
+            .whereGreaterThanOrEqualTo("timeCreated", yesterday)
+            .get()
+            .addOnSuccessListener { documents ->
+                val count = documents.size()
+                binding.dailyInteractionsCount.text = count.toString()
+            }
+    }
+
+    private fun countWeeklyInteractionss() {
+        val weekAgo = Date(System.currentTimeMillis() - 6 * 24 * 60 * 60 * 1000)
+        db.collection("patient-records")
+            .whereGreaterThanOrEqualTo("timeCreated", weekAgo)
+            .get()
+            .addOnSuccessListener { documents ->
+                val count = documents.size()
+                binding.interactionsCount.text = count.toString()
+            }
+    }
+
+    private fun countMontlyInteractions() {
+        val l: Long = System.currentTimeMillis() - 14 * 24 * 60 * 60 * 1000
+        val weekAgo = Date(l)
+        db.collection("patient-records")
+            .whereGreaterThanOrEqualTo("timeCreated", weekAgo)
+            .get()
+            .addOnSuccessListener { documents ->
+                val count = documents.size()
+                binding.interactionsCount.text = count.toString()
+            }
     }
 
 }
